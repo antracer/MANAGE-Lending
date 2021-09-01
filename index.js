ea993834283c53be6c7b1e3a024cb8544330cb41
ANTRACER <otherantracer@gmail.com>
2021-09-01 17:36:01 +0300

    Try to adapt: close one

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+$(window).on('load', function() {
+	let i = 1;
+
+	$('.navbar-toggler-icon').on('click', function(){
+		$('.last-nav-link').hide();
+		if (i == 1) {
+			this.style.backgroundImage = 'url(images/icon-close.svg)';
+			$('.navbar-collapse').css('background-color', '#f2f3f4');
+			i = 2;
+		} else {
+			this.style.backgroundImage = 'url(images/icon-hamburger.svg)';
+			i = 1;
+		}
+	});
+});
 
